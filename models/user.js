e40bbe1dde6ce03bf1f96f9bc17527ab143e15b7
Mathew Johns <reallymathew@gmail.com>
2016-10-10 19:40:49 +0000

    add user schema

---
+++ models/user.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
+
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//adds method to every user object
+UserSchema.methods.validatePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(err, isValid) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, isValid);
+    });
+};
+
+var User = mongoose.model("User", UserSchema);
+
+module.exports = User;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import express from 'express';
+var mongoose = require('mongoose');
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
@@ -10,19 +11,41 @@ const app = express();
 
 app.use(express.static(process.env.CLIENT_PATH));
 
-function runServer() {
-    return new Promise((resolve, reject) => {
-        app.listen(PORT, HOST, (err) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            }
+mongoose.Promise = global.Promise; //why does this make it work?
 
-            const host = HOST || 'localhost';
-            console.log(`Listening on ${host}:${PORT}`);
+var runServer = function(callback) {
+    var databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://localhost/FeelsGood';
+    mongoose
+        .connect(databaseUri)
+        .then(function() {
+            console.log('db connected...');
+            var port = process.env.PORT || 8080;
+            var server = app.listen(port, function() {
+                console.log('Listening on localhost:' + port);
+                if (callback) {
+                    callback(server);
+                    console.log('server running');
+                }
+            })
+        .catch(function(err){
+            console.log(err); 
         });
     });
-}
+};
+
+// function runServer() {
+//     return new Promise((resolve, reject) => {
+//         app.listen(PORT, HOST, (err) => {
+//             if (err) {
+//                 console.error(err);
+//                 reject(err);
+//             }
+
+//             const host = HOST || 'localhost';
+//             console.log(`Listening on ${host}:${PORT}`);
+//         });
+//     });
+// }
 
 if (require.main === module) {
     runServer();
